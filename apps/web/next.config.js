7cc46473604feed21c4bb215d69225a01ef33ec8
Yi-Ru Lin <deduce@gmail.com>
2023-10-12 13:50:50 -0500

    update .vscode settings & next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  reactStrictMode: true,
   experimental: {
     serverComponentsExternalPackages: ["libsql"],
   },
+  transpilePackages: ["ui"],
 };
 
 module.exports = nextConfig
