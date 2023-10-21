87303ae926f042c3a61f789588b5adf4ec753432
Yi-Ru Lin <deduce@gmail.com>
2023-10-21 18:41:59 -0500

    sorting posts according to published date by default

--- apps/web/src/lib/api/post.ts
+++ apps/web/src/lib/api/post.ts
@@ -1,10 +1,11 @@
-import { db , eq, sql } from "@paperknife/database";
+import { db , eq, sql, desc } from "@paperknife/database";
 import { posts as postsTable } from "@paperknife/database/schema/post"
 import type { Post } from "@paperknife/database/types";
 
 export async function getPosts(): Promise<Post[]> {
   const posts = await db.query.posts.findMany({
-    where: eq(postsTable.published, true)
+    where: eq(postsTable.published, true),
+    orderBy: [desc(postsTable.publishedAt)]
   }) as Post[];
 
   return posts;
