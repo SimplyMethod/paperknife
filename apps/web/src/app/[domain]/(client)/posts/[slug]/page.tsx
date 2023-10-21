8851680e1085e26928b6746bc21a3f7f6cb64231
Yi-Ru Lin <deduce@gmail.com>
2023-10-20 19:04:38 -0500

    add client posts page

--- apps/web/src/app/[domain]/(client)/posts/[slug]/page.tsx
+++ apps/web/src/app/[domain]/(client)/posts/[slug]/page.tsx
@@ -1,7 +1,18 @@
-export default function Page() {
+import { getPosts } from "@/lib/api/post"
+
+export default async function Page() {
+  const posts = await getPosts();
+
   return (
     <>
-      HI!
+      {
+        posts.map((post) => (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.content}</p>
+          </div>
+        ))
+      }
     </>
   )
 }
--- apps/web/src/lib/api/post.ts
+++ apps/web/src/lib/api/post.ts
@@ -2,7 +2,7 @@ import { db , eq, sql } from "@paperknife/database";
 import { posts as postsTable } from "@paperknife/database/schema/post"
 import type { Post } from "@paperknife/database/types";
 
-export async function getPosts() {
+export async function getPosts(): Promise<Post[]> {
   const posts = await db.query.posts.findMany({
     where: eq(postsTable.published, true)
   }) as Post[];
